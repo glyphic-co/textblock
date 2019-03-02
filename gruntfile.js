04791153c2987ac179da6b8a18e1d9da9880e926
tinymachine <mihira@tinymachine.net>
2019-03-02 01:46:05 -0800

    Grunt: replace date with version number in banner
    (Prevents meaningless version control changes from being indicated when script is re-minified but no code has changed other than the date.)

--- gruntfile.js
+++ gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
 
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '/*! <%= pkg.name %> <%= pkg.version %> */'
       },
       textblock: {
         options: {
